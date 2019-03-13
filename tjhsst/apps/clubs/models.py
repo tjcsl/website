e7ae585ef7bccef011297ba383858b3d137b632e
Keegan Lanzillotta <swfanboy01@gmail.com>
2019-03-13 15:31:23 -0400

    Update models.py

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -3,10 +3,6 @@ from django.db import models
 # Create your models here.
 class Clubs(models.Model):
     name = models.CharField(max_length=100, primary_key = True)
-    image = models.ImageField(upload_to = "club_photos/")
+    image = models.ImageField(upload_to = "club_photos/", height_field = height, width_field = width)
     description = models.CharField(max_length=5000)
-    link = models.CharField(max_length=200, blank = True, height_field = height, width_field = width)
-    
-    width = models.IntegerField()
-    height = models.IntegerField()
-    
+    link = models.CharField(max_length=200, blank = True)    
