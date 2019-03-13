54021cfb5ce2c2236b105b3366cf083848625e69
Keegan Lanzillotta <swfanboy01@gmail.com>
2019-03-13 15:33:18 -0400

    Create width and height fields

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Clubs(models.Model):
     name = models.CharField(max_length=100, primary_key = True)
+    height = models.IntegerField()
+    width = models.IntegerField()
     image = models.ImageField(upload_to = "club_photos/", height_field = height, width_field = width)
     description = models.CharField(max_length=5000)
     link = models.CharField(max_length=200, blank = True)    
