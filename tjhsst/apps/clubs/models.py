d4bd8be7489d3ef4694bdd32a22475f1a1db8960
Akash Bhave <akashbbhave@gmail.com>
2019-03-13 15:29:25 -0400

    Replace ';' with ':' in clubs
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Clubs(models.Model);
+class Clubs(models.Model):
     name = models.CharField(max_length=100, primary_key = True)
     image = models.ImageField(upload_to = "club_photos/")
     description = models.CharField(max_length=5000)
