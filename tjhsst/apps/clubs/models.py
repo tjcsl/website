afb8b67e194bb0c7b450f3d4078eff99960153aa
unknown <pmd7211@gmail.com>
2019-03-13 15:11:29 -0400

    testing with latest update

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
-
+class Club(models.Model):
+    name = models.Charfield(max_length=100, primary_key=True)
+    description = models.Charfield(max_length=5000)
+    link = models.Charfield(max_length=200, blank=True)
+    image = models.ImageField(upload_to="club_photos/", height_field=height, width_field=width)
+    width = models.IntegerField()
+    height = models.IntegerField()
