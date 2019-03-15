b3fa5ca3684a0776e864a6e1e54939bfb0ba0d04
Akash Bhave <akashbbhave@gmail.com>
2019-03-15 15:36:40 -0400

    Delete width and height fields from clubs/models
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -5,7 +5,7 @@ class Club(models.Model):
     name = models.CharField(max_length=100, primary_key = True)
     height = models.IntegerField()
     width = models.IntegerField()
-    image = models.ImageField(upload_to = "club_photos/", height_field = height, width_field = width)
+    image = models.ImageField(upload_to = "club_photos/")
     description = models.CharField(max_length=5000)
     link = models.CharField(max_length=200, blank = True)    
     
