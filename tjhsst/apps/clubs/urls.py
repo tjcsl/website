c0b5b6fcb93a9826091fe3516615e7552d18b1c5
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-03-27 17:14:55 -0400

    Houston, we have a page for each club
    We still need one to list all the clubs, though

--- tjhsst/apps/clubs/urls.py
+++ tjhsst/apps/clubs/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "clubs"
 
 urlpatterns = [
+    path("<str:club_url>", views.show, name = "show"),
 ]
 
--- tjhsst/apps/clubs/views.py
+++ tjhsst/apps/clubs/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Club, Keyword, Category
 
 # Create your views here.
+
+def show(request, club_url):
+    club = get_object_or_404(Club, url = club_url)
+    return render(request, "clubs/show.html", context = {"club": club})
 
