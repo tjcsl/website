46238b33970ef788e3c56dc2440b9b4a34e7d98d
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-03-29 14:30:56 -0400

    Fix more model stuff
    Hopefully this is the last time we will have to delete migrations

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -5,7 +5,7 @@ from django.core.validators import RegexValidator
 class Club(models.Model):
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length=100, unique = True)
-    url = models.CharField(max_length=10, unique = True, validators=[RegexValidator(regex="^[a-zA-Z0-9_]+$", message="Only alphanumeric and underscores allowed")])
+    url = models.CharField(max_length=10, unique = True, validators=[RegexValidator(regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
     height = models.IntegerField()
     width = models.IntegerField()
     image = models.ImageField(upload_to = "club_photos/", height_field = "height", width_field = "width")
@@ -19,13 +19,17 @@ class Club(models.Model):
         return self.name
 
 class Keyword(models.Model):
-    name = models.CharField(max_length=20, primary_key = True)
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length=20, unique = True)
+    url = models.CharField(max_length=20, unique = True, validators=[RegexValidator(regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
     def __str__(self):
         return self.name
     
 class Category(models.Model):
-    name = models.CharField(max_length=20, primary_key = True)
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length=20, unique = True)
+    url = models.CharField(max_length=20, unique = True, validators=[RegexValidator(regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
     def __str__(self):
         return self.name
