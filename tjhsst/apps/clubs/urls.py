5ac167e4704aa1a4565a2365f2265eea1f58560e
Keegan <swfanboy01@gmail.com>
2019-03-29 14:39:23 -0400

    Make category and keyword pages

--- tjhsst/apps/clubs/urls.py
+++ tjhsst/apps/clubs/urls.py
@@ -6,5 +6,7 @@ app_name = "clubs"
 
 urlpatterns = [
     path("<str:club_url>", views.show, name = "show"),
+    path("category/<str:category_url>", views.show_category, name="show_category"),
+    path("keyword/<str:keyword_url>", views.show_keyword, name="show_keyword"),
 ]
 
--- tjhsst/apps/clubs/views.py
+++ tjhsst/apps/clubs/views.py
@@ -8,3 +8,10 @@ def show(request, club_url):
     club = get_object_or_404(Club, url = club_url)
     return render(request, "clubs/show.html", context = {"club": club})
 
+def show_category(request, category_url):
+    category = get_object_or_404(Category, url = category_url)
+    return render(request, "clubs/categories/show.html", context = {"category": category})
+
+def show_keyword(request, keyword_url):
+    keyword = get_object_or_404(Keyword, url = keyword_url)
+    return render(request, "clubs/keywords/show.html", context = {"keyword": keyword})
