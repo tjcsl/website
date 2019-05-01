d1bcf4994abcc4e7716c6d24e481ea1372d92ecc
Keegan <swfanboy01@gmail.com>
2019-05-01 15:52:16 -0400

    Make Club image+keywords optional

--- tjhsst/apps/clubs/models.py
+++ tjhsst/apps/clubs/models.py
@@ -7,13 +7,13 @@ class Club(models.Model):
     name = models.CharField(max_length=100, unique = True)
     url = models.CharField(max_length=20, unique = True, validators=[RegexValidator(regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
-    image = models.ImageField(upload_to = "club_photos/", null = True)
+    image = models.ImageField(upload_to = "club_photos/", null = True, blank=True)
 
     description = models.CharField(max_length=5000, default="This club page has not yet been filled out.")
     link = models.CharField(max_length=200, blank = True)
 
     category = models.ForeignKey("Category", related_name="clubs", on_delete=models.SET_NULL, blank = True, null = True)
-    keywords = models.ManyToManyField("Keyword", related_name="clubs")
+    keywords = models.ManyToManyField("Keyword", related_name="clubs", blank=True)
     admins = models.ManyToManyField("users.User", related_name="clubs")
 
     def __str__(self):
