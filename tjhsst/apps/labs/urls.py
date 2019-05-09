0ae571446939163b606035b5cdb2785d3fc3b345
Invinceaman <invinceaman023@gmail.com>
2019-05-09 14:32:00 -0400

    Start find labs page

--- tjhsst/apps/labs/urls.py
+++ tjhsst/apps/labs/urls.py
@@ -6,6 +6,7 @@ app_name = "labs"
 
 urlpatterns = [
     path("", views.index, name = "index"),
+    path("find", views.show_courses, name="find_by_courses"),
     path("<str:lab_url>", views.show, name = "show"),
     path("course/<str:course_url>", views.show_course, name="show_course"),
 ]
