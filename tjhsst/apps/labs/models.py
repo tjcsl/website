3b4808528166cd79179aaf4913ed698acdc898b1
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-05-09 15:25:00 -0400

    Delete height and width fields on labs; apply changes I forgot to migrate

--- tjhsst/apps/labs/models.py
+++ tjhsst/apps/labs/models.py
@@ -7,9 +7,7 @@ class Lab(models.Model):
     name = models.CharField(max_length=100, unique = True)
     url = models.CharField(max_length=20, unique = True, validators=[RegexValidator(regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
-    height = models.IntegerField(null = True)
-    width = models.IntegerField(null = True)
-    image = models.ImageField(upload_to = "lab_photos/", height_field = "height", width_field = "width", null = True, blank = True)
+    image = models.ImageField(upload_to = "lab_photos/", null = True, blank = True)
 
     description = models.CharField(max_length=5000, default="This club page has not yet been filled out.")
     link = models.CharField(max_length=200, blank = True)
