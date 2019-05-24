b21f6b58e2416fbe4add205be09c78b2349a3071
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-05-24 14:59:26 -0400

    Don't display the search field on home

--- tjhsst/apps/context_processors.py
+++ tjhsst/apps/context_processors.py
@@ -3,6 +3,8 @@ from django.urls import NoReverseMatch
 
 def set_search_field_vars(request):
     app_name = request.resolver_match.app_name
+    if app_name == "home":
+        return {}
     try:
         search_url = reverse(app_name + ":index")
     except NoReverseMatch:
