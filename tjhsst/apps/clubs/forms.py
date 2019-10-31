e642f259af3360b9203fdd9bd848f240c09adc98
anonymoose2 <anonymoose2@users.noreply.github.com>
2019-10-31 18:35:33 -0400

    Fix activity ID field

--- tjhsst/apps/clubs/forms.py
+++ tjhsst/apps/clubs/forms.py
@@ -5,13 +5,12 @@ from .models import Category, Club, Keyword
 
 class ClubForm(forms.ModelForm):
     link = forms.URLField(required=False)
-    activity_id = forms.IntegerField(required=False)
     category = forms.ModelChoiceField(Category.objects.all(), required=True, empty_label=None)
     keywords = forms.ModelMultipleChoiceField(Keyword.objects.all(), required=False)
 
     class Meta:
         model = Club
-        fields = ["name", "image", "description", "link", "category", "keywords"]
+        fields = ["name", "image", "description", "link", "category", "keywords", "activity_id"]
         widgets = {"description": forms.Textarea(attrs={"cols": 40, "rows": 3})}
 
 
