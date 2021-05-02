04cad4e385a0afd9e7f00900dc6d760ef3dd23e5
lauren <pmd7211@gmail.com>
2021-05-01 22:50:12 -0400

    labs: add testimonials and projects

--- tjhsst/apps/labs/models.py
+++ tjhsst/apps/labs/models.py
@@ -32,6 +32,36 @@ class Lab(models.Model):
         return self.name
 
 
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    description = models.CharField(max_length=5000)
+    image = models.ImageField(upload_to="project_photos/", null=True, blank=True)
+
+    authors = models.ManyToManyField("users.User", related_name="senior_projects_authored")
+    labs = models.ManyToManyField("Lab", related_name="senior_projects_sponsored")
+
+    def __str__(self):
+        return "{}: {}".format(self.display_labs(), self.name)
+
+    def display_labs(self):
+        return " ,".join([lab.name for lab in self.labs.all()])
+
+    def display_authors(self):
+        return " ,".join(["{} {}".format(a.first_name, a.last_name) for a in self.authors.all()])
+
+
+class Testimonial(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey("users.User", related_name="testimonials_written", on_delete=models.SET_NULL, null=True, blank=True)
+    lab = models.ForeignKey("Lab", related_name="testimonials_set", on_delete=models.CASCADE)
+    content = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return "{}: {}...".format(self.lab, self.content[:40])
+
+
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
